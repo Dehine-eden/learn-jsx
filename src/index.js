ea485f6bb282895f7a4cba772017c5fd8b80499b
Dehine-eden <119399020+Dehine-eden@users.noreply.github.com>
2023-02-10 16:39:16 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My favourite books</h1>
+    <ul>
+      <li>Emanda</li>
+      <li>Atomic Habits</li>
+      <li>Eat that frog</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
